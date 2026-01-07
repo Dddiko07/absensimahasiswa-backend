f9c36d8213390908d83d26a9780f478eefd5a565
Dddiko07 <diko.23285@mhs.unesa.ac.id>
2026-01-07 20:28:47 +0700

    fix serverless vercel api

--- api/index.js
+++ api/index.js
@@ -1,2 +1,15 @@
-const app = require("../app");
+const express = require("express");
+const cors = require("cors");
+
+const absensiRoutes = require("../routes/absensi");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// ⬅️ ROUTE UTAMA
+app.use("/", absensiRoutes);
+
+// ⬅️ EXPORT APP (WAJIB)
 module.exports = app;
