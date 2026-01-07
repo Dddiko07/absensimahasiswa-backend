697cae388118bdf0123efcb0a4adec98847fd9f2
Dddiko07 <diko.23285@mhs.unesa.ac.id>
2026-01-07 20:46:58 +0700

    fix postgres for vercel

--- api/absensi.js
+++ api/absensi.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-
 const absensiRoutes = require("../routes/absensi");
 
 const app = express();
@@ -8,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ⬇️ penting: route root karena sudah di /api/absensi
+// karena file ini = /api/absensi
 app.use("/", absensiRoutes);
 
 module.exports = app;
